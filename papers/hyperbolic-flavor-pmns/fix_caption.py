c9573ba3ac2c4caad4b9323ed45caff123cc5b9e
Marvin Gentry <drmlgentry@protonmail.com>
2026-03-13 16:42:45 -0700

    PMNS paper: final submission version; clean repo artifacts and session scripts

--- papers/hyperbolic-flavor-pmns/fix_caption.py
+++
@@ -1,6 +0,0 @@
-﻿path = r'C:\dev\framework\papers\hyperbolic-flavor-pmns\gentry-hyperbolic-flavor-pmns.tex'
-with open(path, 'r', encoding='utf-8-sig') as f: tex = f.read()
-
-# Find and show the fig2 caption to diagnose
-i = tex.find('fig2_geodesic_axes')
-print(repr(tex[i:i+400]))
